82dd55306da858571c7186260a89af340ea90e89
wilbhall <joseph.leichter@gmail.com>
2017-02-08 18:50:05 -0500

    Initial Commit

---
+++ mvc-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mvc-test
+//
+//  Created by Joseph Leichter on 2/8/17.
+//  Copyright © 2017 Joseph Leichter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
